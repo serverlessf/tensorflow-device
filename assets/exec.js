79de5ea40e3624d924d695ce0a9eee2d99bffa75
Subhajit Sahu <wolfram77@gmail.com>
2019-04-03 12:59:41 +0530

    cool exec

---
+++ assets/exec.js
@@ -0,0 +1,44 @@
+const $h2 = document.querySelector('h2');
+const $form = document.querySelector('form');
+const $cmd = document.querySelector('#cmd');
+const $interval = document.querySelector('#interval');
+const $stdout = document.querySelector('#stdout');
+const $stderr = document.querySelector('#stderr');
+var options = {};
+
+
+
+function searchParse(search) {
+  var search = search.substring(1);
+  return search? JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"')
+  .replace(/&/g, '","').replace(/=/g,'":"') + '"}'):{};
+}
+
+function onReady() {
+  var o = searchParse(location.search);
+  console.log('onReady()', o);
+  if(o.cmd) $cmd.value = o.cmd;
+  if(o.interval) $interval.value = o.interval;
+  m.render($h2, o.process? m([o.process, m('small', o.engine||'')]):null);
+  document.body.className = o.process? 'process':'os';
+  return o;
+}
+
+async function render(cmd, o) {
+  var url = o.process? `/process/${o.process}/exec`:'/exec';
+  m.request({method: 'POST', url, data: {cmd}}).then((data) => {
+    m.render($stdout, data.stdout);
+    m.render($stderr, data.stderr);
+  }, (err) => {
+    m.render($stderr, err.stderr);
+  });
+}
+
+
+
+options = onReady();
+if($cmd.value) render($cmd.value, options);
+$cmd.onchange = () => render($cmd.value, options) && false;
+setInterval(() => {
+  if(parseInt($interval.value)) render($cmd.value, options);
+}, 1000);
--- assets/os.js
+++ assets/os.js
@@ -31,7 +31,7 @@ async function render() {
     }
     return trs;
   }, []));
-};
+}
 
 
 
