d312fb3e55b92571a7d6b49188f412124b554f0a
Subhajit Sahu <wolfram77@gmail.com>
2019-04-03 21:00:51 +0530

    running stopped created total

--- assets/sdata.js
+++ assets/sdata.js
@@ -28,6 +28,19 @@ function infoMount(str) {
   return out;
 }
 
+function stateCount(name, ps) {
+  var total = 0, created = 0, running = 0, exited = 0;
+  for(var p of ps) {
+    var pname = p.Names[0].substring(1), s = p.State;
+    if(pname.replace(/\..*$/, '')!==name) continue;
+    if(s==='created') created++;
+    if(s==='running') running++;
+    else if(s==='exited') exited++;
+    total++;
+  }
+  return {total, created, running, exited};
+}
+
 function searchParse(search) {
   var search = search.substring(1);
   return search? JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"')
@@ -53,17 +66,11 @@ async function request(o) {
   var sp = m.request({method: 'GET', url: '/service/'+name});
   var psp = m.request({method: 'GET', url: '/process?all=1'});
   var [s, ps] = await Promise.all([sp, psp]);
-  var running = 0, stopped = 0;
-  for(var p of ps) {
-    var pname = p.Names[0].substring(1), st = p.State.Status;
-    if(pname.replace(/\..*$/, '')!==name) continue;
-    if(st==='running') running++;
-    else if(st==='stopped') stopped++;
-  }
+  var {total, created, running, exited} = stateCount(name, ps);
   m.render($h2, [s.name, m('div', m('small', s.engine))]);
   m.render($state, m('tr', [
-    m('td', moment(s.created).fromNow()),
-    m('td', running), m('td', stopped),
+    m('td', moment(s.created).fromNow()), m('td', running),
+    m('td', exited), m('td', created), m('td', total),
   ]));
   m.render($policy, m('tr', [
     m('td', s.workdir),
@@ -81,7 +88,14 @@ async function request(o) {
   )))));
 }
 
-function onButton(fn, pre, o) {
+function onRun(o) {
+  m.request({method: 'POST', url: `/service/${o.service}/run`}).then((data) => {
+    iziToast.success({message: `Ran service ${o.service} as ${data.name}`});
+  }, (err) => iziToast.error({message: err.message}));
+  return false;
+}
+
+function onButton(o, fn, pre) {
   m.request({method: 'POST', url: `/service/${o.service}/${fn}`}).then((data) => {
     iziToast.success({message: `${pre} service ${o.service}`});
   }, (err) => iziToast.error({message: err.message}));
@@ -93,10 +107,10 @@ function onButton(fn, pre, o) {
 options = onReady();
 request(options);
 setInterval(() => request(options), 5000);
-$run.onclick = () => onButton('run', 'Ran', options);
-$stop.onclick = () => onButton('stop', 'Stopped', options);
-$kill.onclick = () => onButton('kill', 'Killed', options);
-$restart.onclick = () => onButton('restart', 'Restarted', options);
-$pause.onclick = () => onButton('pause', 'Paused', options);
-$unpause.onclick = () => onButton('unpause', 'Unpaused', options);
-$remove.onclick = () => onButton('remove', 'Removed', options);
+$run.onclick = () => onRun(options);
+$stop.onclick = () => onButton(options, 'stop', 'Stopped');
+$kill.onclick = () => onButton(options, 'kill', 'Killed');
+$restart.onclick = () => onButton(options, 'restart', 'Restarted');
+$pause.onclick = () => onButton(options, 'pause', 'Paused');
+$unpause.onclick = () => onButton(options, 'unpause', 'Unpaused');
+$remove.onclick = () => onButton(options, 'remove', 'Removed');
