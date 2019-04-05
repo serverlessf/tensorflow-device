0e1d782df091ef0638935ebefe91b71c9464e17b
Subhajit Sahu <wolfram77@gmail.com>
2019-04-06 00:11:51 +0530

    config should be simple

--- web/config.js
+++ web/config.js
@@ -1,56 +1,54 @@
+const net = require('extra-net');
 const path = require('path');
 const fs = require('fs');
 
 
 
+const E = process.env;
 const ROOT = path.dirname(require.main.filename);
+const PORT = E['PORT']||'8000';
+const IP = net.address().address;
+const ADDRESS = IP+':'+PORT;
+const QUERY = E['QUERY']||'';
+
 const CONFIGFILE = 'config.json';
 const ARRAYKEYS = ['ports', 'mounts', 'env', 'cmd'];
 const DEFAULTPATH = path.join(ROOT, 'config');
 const DEFAULTENGINE = 'python:3';
 const DEFAULTS = new Map();
-const NOP = () => 0;
 
 
 
 function defaults(o) {
   ARRAYKEYS.forEach(k => { if(typeof o[k]==='string') o[k] = o[k].split(';'); });
-  console.log('config.defaults', o);
   o = Object.assign(DEFAULTS.get((o.engine||DEFAULTENGINE).replace(/\W/g, '_')), o);
-  console.log('config.defaults', o);
   o = Object.assign(DEFAULTS.get('index'), o);
-  console.log('config.defaults', o);
-  // for(var k in o.env) o.env[k] = o.env[k].replace(/\$name/g, o.name);
-  for(var k in o.env) o.env[k] = o.env[k].replace(/\$port/g, o.ports[0]);
   o.created = o.created||new Date();
   return o;
 }
 
-function write(dir, value) {
+async function write(dir, value) {
   var file = path.join(dir, CONFIGFILE);
-  fs.writeFile(file, JSON.stringify(value, null, 2), NOP);
+  await fs.writeFile(file, JSON.stringify(value, null, 2));
 }
 
-function read(dir) {
+async function read(dir) {
   var file = path.join(dir, CONFIGFILE);
-  var o = fs.existsSync(file)? JSON.parse(fs.readFileSync(file, 'utf8')):{};
-  return defaults(Object.assign(o, {path: dir}));
+  var o = fs.existsSync(file)? JSON.parse(await fs.readFile(file, 'utf8')):{};
+  return Object.assign(o, {path: dir});
 }
-
-function readAll(dir, configs={}) {
-  for(var name of fs.readdirSync(dir))
-    configs[name] = Object.assign({}, read(path.join(dir, name)), {name});
-  return configs;
-}
+exports.ROOT = ROOT;
+exports.PORT = PORT;
+exports.IP = IP;
+exports.ADDRESS = ADDRESS;
+exports.QUERY = QUERY;
 exports.defaults = defaults;
 exports.write = write;
 exports.read = read;
-exports.readAll = readAll;
 
 
 
 for(var file of fs.readdirSync(DEFAULTPATH)) {
-  var fname = file.substring(0, file.length-path.extname(file).length);
   var config = JSON.parse(fs.readFileSync(path.join(DEFAULTPATH, file), 'utf8'));
-  DEFAULTS.set(fname, config);
+  DEFAULTS.set(path.parse(file).name, config);
 }
