24b9515146366e181522d64e8e2e90325ec3ea9c
Subhajit Sahu <wolfram77@gmail.com>
2019-04-05 23:44:27 +0530

    fetch does not need root, name

--- web/fetch.js
+++ web/fetch.js
@@ -15,42 +15,33 @@ async function dirDehusk(dir) {
   await fs.remove(temp);
 }
 
-async function fetchGit(url, dir, name=null) {
-  var name = name||path.parse(url).file;
-  var pkg = path.join(dir, name);
-  if(fs.existsSync(pkg)) await fs.remove(pkg);
+async function fetchGit(dir, url) {
   var repo = url.replace(/#.*/, ''), branch = url.substring(repo.length+1)||'master';
-  var cmd = `git clone --single-branch --branch ${branch} --depth=1 ${repo} ${name}`;
+  var cmd = `git clone --single-branch --branch ${branch} --depth=1 ${repo} .`;
   await cp.exec(cmd, {cwd: dir});
 }
 
-async function fetchUrl(url, dir, name=null) {
-  var name = name||path.parse(url).file;
-  var pkg = path.join(dir, name);
-  var out = path.join(pkg, path.basename(url));
-  if(fs.existsSync(pkg)) await fs.remove(pkg+'/*');
-  fs.mkdirSync(pkg, {recursive: true});
-  await download(url, pkg, {extract: true});
+async function fetchUrl(dir, url) {
+  var out = path.join(dir, path.basename(url));
+  await fs.mkdir(dir);
+  await download(url, dir, {extract: true});
   await fs.remove(out);
-  await dirDehusk(pkg);
+  await dirDehusk(dir);
 }
 
-async function fetchFile(file, dir, name=null) {
-  var name = name||path.parse(file.name).name;
-  var pkg = path.join(dir, name);
-  var out = path.join(pkg, path.basename(file.name));
-  if(fs.existsSync(pkg)) await fs.remove(pkg+'/*');
-  fs.mkdirSync(pkg, {recursive: true});
-  await new Promise((fres, frej) => file.mv(out, (err) => err? frej(err):fres()));
-  await decompress(out, pkg);
+async function fetchFile(dir, file) {
+  var out = path.join(dir, path.basename(file.name));
+  await fs.mkdir(dir);
+  await new Promise((fres, frej) => file.mv(out, e => e? frej(e):fres()));
+  await decompress(out, dir);
   await fs.remove(out);
-  await dirDehusk(pkg);
+  await dirDehusk(dir);
 }
 
-function fetch(options, dir, name=null) {
+function fetch(dir, options) {
   var {git, url, file} = options;
-  if(git) return fetchGit(git, dir, name);
-  if(url) return fetchUrl(url, dir, name);
-  return fetchFile(file, dir, name);
+  if(git) return fetchGit(dir, git);
+  if(url) return fetchUrl(dir, url);
+  return fetchFile(dir, file);
 }
 module.exports = fetch;
