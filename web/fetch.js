adebee89fb36ed5f31743c51eb674da4e57c94cb
Subhajit Sahu <wolfram77@gmail.com>
2019-04-05 23:49:35 +0530

    fetch, if exists, clean, else mkdirp

--- web/fetch.js
+++ web/fetch.js
@@ -23,7 +23,6 @@ async function fetchGit(dir, url) {
 
 async function fetchUrl(dir, url) {
   var out = path.join(dir, path.basename(url));
-  await fs.mkdir(dir);
   await download(url, dir, {extract: true});
   await fs.remove(out);
   await dirDehusk(dir);
@@ -31,15 +30,16 @@ async function fetchUrl(dir, url) {
 
 async function fetchFile(dir, file) {
   var out = path.join(dir, path.basename(file.name));
-  await fs.mkdir(dir);
   await new Promise((fres, frej) => file.mv(out, e => e? frej(e):fres()));
   await decompress(out, dir);
   await fs.remove(out);
   await dirDehusk(dir);
 }
 
-function fetch(dir, options) {
+async function fetch(dir, options) {
   var {git, url, file} = options;
+  if(fs.existsSync(dir)) await fs.remove(dir+'/*');
+  else await fs.mkdirp(dir);
   if(git) return fetchGit(dir, git);
   if(url) return fetchUrl(dir, url);
   return fetchFile(dir, file);
