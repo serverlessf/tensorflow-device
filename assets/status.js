6fdad3c9bf1c71ee64c9af461d5e6ddb770e54d2
Subhajit Sahu <wolfram77@gmail.com>
2019-04-13 16:16:32 +0530

    start status.html

--- assets/status.js
+++ assets/status.js
@@ -1,10 +1,27 @@
 const $editor = document.querySelector('#editor');
-const editor = ace.edit('editor');
+const editor = ace.edit('editor', {
+  theme: 'ace/theme/monakai',
+  mode: 'ace/mode/json',
+  minLines: 30,
+  maxLines: 30,
+});
 
 
 
+function searchParse(search) {
+  var search = search.substring(1);
+  return search? JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"')
+  .replace(/&/g, '","').replace(/=/g,'":"') + '"}'):{};
+}
+
+function onReady() {
+  var o = searchParse(location.search);
+  console.log('onReady()', o);
+  $upload.setAttribute('href', `/upload.html?image=${o.image}&from=${o.from}&update=1`);
+  $logs.setAttribute('href', `/logs.html?image=${o.image}&from=${o.from}`);
+  return o;
+}
 
 function setup() {
-  editor.setTheme('ace/theme/monakai');
-  editor.session.setMode('ace/mode/json');
 }
+setup();
