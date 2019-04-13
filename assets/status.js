826e44739c023b3acad8b9fd2ebb53b228269abd
Subhajit Sahu <wolfram77@gmail.com>
2019-04-13 17:29:17 +0530

    GET, POST /status workinh with UI, but cant remove keys (must set null)

--- assets/status.js
+++ assets/status.js
@@ -1,10 +1,14 @@
+const $write = document.querySelector('#write');
+const $get = document.querySelector('#get');
+const $post = document.querySelector('#post');
 const $editor = document.querySelector('#editor');
 const editor = ace.edit('editor', {
-  theme: 'ace/theme/monakai',
+  theme: 'ace/theme/github',
   mode: 'ace/mode/json',
   minLines: 30,
   maxLines: 30,
 });
+var options = {};
 
 
 
@@ -17,11 +21,40 @@ function searchParse(search) {
 function onReady() {
   var o = searchParse(location.search);
   console.log('onReady()', o);
-  $upload.setAttribute('href', `/upload.html?image=${o.image}&from=${o.from}&update=1`);
-  $logs.setAttribute('href', `/logs.html?image=${o.image}&from=${o.from}`);
+  $write.checked = !!o.write;
+  onGet(o);
   return o;
 }
 
-function setup() {
+function url(o) {
+  if(o.image) return `/image/${o.image}/status`;
+  if(o.container) return `/container/${o.container}/status`;
+  return `/status`;
 }
-setup();
+
+function message(o) {
+  if(o.image) return `Status saved to image ${o.image}`;
+  if(o.container) return `Status saved to container ${o.container}`;
+  return `Status saved to device`;
+}
+
+function onGet(o) {
+  var write = o.write? '?write=1':'';
+  m.request({method: 'GET', url: url(o)+write}).then((data) => {
+    editor.setValue(JSON.stringify(data, null, 2)); editor.gotoLine(1);
+  }, (err) => iziToast.error({message: err.message}));
+  return false;
+}
+
+function onPost(o) {
+  var data = JSON.parse(editor.getValue());
+  m.request({method: 'POST', url: url(o), data}).then(() => {
+    iziToast.success({message: message(o)});
+  }, (err) => iziToast.error({message: err.message}));
+  return false;
+}
+
+options = onReady();
+$write.onchange = () => options.write = $write.checked;
+$get.onclick = () => onGet(options);
+$post.onclick = () => onPost(options);
