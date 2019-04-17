aaa40781fd0e0bf8ac38b0273a145faab514c48c
Subhajit Sahu <wolfram77@gmail.com>
2019-04-17 07:09:34 +0530

    :mushroom: torefactor: config.read() can take dir, instead of file

--- src/config.js
+++ src/config.js
@@ -3,49 +3,23 @@ const path = require('path');
 
 
 
-const COMMON = {
-  version: 0,
-  expose: [8000],
-  from: 'python:3.7-slim',
-  workdir: '/app',
-};
-const SPECIFIC = {
-  node: {
-    run: ['npm install'],
-    cmd: ['npm', 'start'],
-  },
-  python: {
-    run: [
-      'if test -e requirements.txt; then pip install -r requirements.txt; fi',
-      'if test -e setup.py; then python setup.py; fi'
-    ],
-    cmd: ['python', 'main.py'],
-  },
-  tensorflow: {
-    workdir: '/models/model',
-    expose: [8500, 8501],
-    env: {'MODEL_NAME': 'model'},
-  },
-};
+const FILE = 'config.json';
 
 
-
-function defaults(value) {
-  var o = Object.assign({}, COMMON, value);
-  var from = o.from.replace(/\W.*/, '');
-  o = Object.assign(o, SPECIFIC[from], o);
-  o.ctime = o.atime = o.mtime = new Date();
-  return o;
-}
 
 async function read(file) {
-  return fs.existsSync(file)? JSON.parse(await fs.readFile(file, 'utf8')):{};
+  // var file = path.join(dir, FILE);
+  if(!fs.existsSync(file)) return {};
+  var data = await fs.readFile(file, 'utf8');
+  return JSON.parse(data);
 }
 
 async function write(file, value) {
+  // var file = path.join(dir, FILE);
   await fs.mkdirp(path.dirname(file));
-  await fs.writeFile(file, JSON.stringify(Object.assign(await read(file), value), null, 2));
+  var value = Object.assign(await read(file), value);
+  await fs.writeFile(file, JSON.stringify(value, null, 2));
 }
+exports.FILE = FILE;
 exports.write = write;
 exports.read = read;
-exports.defaults = defaults;
--- src/image.js
+++ src/image.js
@@ -14,6 +14,40 @@ const DOCKERFILE = 'Dockerfile';
 const CONFIGFILE = 'config.json';
 const BUILDLOG = 'build.log';
 const docker = new Docker();
+const COMMON = {
+  version: 0,
+  expose: [8000],
+  from: 'python:3.7-slim',
+  workdir: '/app',
+};
+const SPECIFIC = {
+  node: {
+    run: ['npm install'],
+    cmd: ['npm', 'start'],
+  },
+  python: {
+    run: [
+      'if test -e requirements.txt; then pip install -r requirements.txt; fi',
+      'if test -e setup.py; then python setup.py; fi'
+    ],
+    cmd: ['python', 'main.py'],
+  },
+  tensorflow: {
+    workdir: '/models/model',
+    expose: [8500, 8501],
+    env: {'MODEL_NAME': 'model'},
+  },
+};
+
+
+
+function defaults(value) {
+  var o = Object.assign({}, COMMON, value);
+  var from = o.from.replace(/\W.*/, '');
+  o = Object.assign(o, SPECIFIC[from], o);
+  o.ctime = o.atime = o.mtime = new Date();
+  return o;
+}
 
 
 
@@ -107,7 +141,7 @@ async function ls(options) {
 
 async function build(id, dir, options) {
   console.log(options);
-  var o = config.defaults(options);
+  var o = defaults(options);
   var df = path.join(dir, DOCKERFILE);
   await fs.writeFile(df, dockerFile(o));
   var app = path.join(ROOT, id);
