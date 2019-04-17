1ed9e24b1c9e861d443b75364dad3d9a6a963412
Subhajit Sahu <wolfram77@gmail.com>
2019-04-17 23:40:49 +0530

    addition: POST to QUERYADDR on update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('extra-fs');
 const decompress = require('extra-decompress');
 const cp = require('extra-cp');
 const boolean = require('boolean');
+const needle = require('needle');
 const http = require('http');
 const path = require('path');
 const os = require('os');
@@ -14,11 +15,46 @@ const device = require('./src/device');
 
 
 
+const E = process.env;
 const ASSETS = path.join(__dirname, 'assets');
+const STATUSRATE = parseInt(E['STATUSRATE']||'10000', 10);
 const app = express();
 const server = http.createServer(app);
 
 
+
+async function devicePost(addr=device.QUERYADDR) {
+  if(!addr) return;
+  var o = await device.status();
+  var scope = o.scope||'default';
+  needle('post', `${addr}/table/${scope}.device/${o.id}`, o, {json: true});
+}
+
+async function imagePost(id, addr=device.QUERYADDR) {
+  if(!addr) return;
+  var o = await image.status(id);
+  var scope = o.scope||'default';
+  needle('post', `${q}/table/${scope}.image/${o.id}`, o, {json: true});
+}
+
+async function containerPost(id, addr=device.QUERYADDR) {
+  if(!addr) return;
+  var o = await container.status(id);
+  var scope = o.scope||'default';
+  needle('post', `${q}/table/${scope}.container/${o.id}`, o, {json: true});
+}
+
+
+
+async function onInterval() {
+  var q = device.QUERYADDR;
+  if(!q) return;
+  var d = await device.status();
+  var scope = d.scope||'default';
+  needle('post', `${q}/table/${scope}.device/${d.id}`, d, {json: true});
+  
+}
+setInterval(onInterval, STATUSRATE);
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -36,11 +72,13 @@ app.get('/status', express.async(async (req, res) => {
 }));
 app.post('/status', express.async(async (req, res) => {
   res.json(await device.setStatus(req.body));
+  devicePost();
 }));
 
 app.post('/exec', (req, res) => {
   var {cmd} = req.body;
   cp.exec(cmd).then(o => res.json(o), o => res.json(o));
+  devicePost();
 });
 
 app.get('/image', express.async(async (req, res) => {
@@ -58,16 +96,19 @@ app.post('/image', express.async(async (req, res) => {
   var out = await image.build(id, tmp, o);
   await fs.remove(tmp);
   res.json(out);
+  imagePost(id);
 }));
 app.post('/image/:id/run', express.async(async (req, res) => {
   var {id} = req.params, {name} = req.body;
   name = name||(id+'.'+dockerNames.getRandomName());
   var {stdout} = await image.run(id, name, req.body);
   res.json({id: stdout.trim(), name});
+  imagePost(id);
 }));
 app.delete('/image/:id', express.async(async (req, res) => {
   var {id} = req.params;
   res.json(await image.remove(id, req.body));
+  imagePost(id);
 }));
 app.get('/image/:id/status', express.async(async (req, res) => {
   var {id} = req.params, write = boolean(req.body.write);
@@ -76,6 +117,7 @@ app.get('/image/:id/status', express.async(async (req, res) => {
 app.post('/image/:id/status', express.async(async (req, res) => {
   var {id} = req.params;
   res.json(await image.setStatus(id, req.body));
+  imagePost(id);
 }));
 app.get('/image/:id/logs', express.async(async (req, res) => {
   var {id} = req.params, {stderr} = req.body;
@@ -84,6 +126,7 @@ app.get('/image/:id/logs', express.async(async (req, res) => {
 app.all('/image/:id/:action', express.async(async (req, res) => {
   var {id, action} = req.params;
   res.json(await image.command(id, action, true, req.body));
+  imagePost(id);
 }));
 
 app.get('/container', express.async(async (req, res) => {
@@ -92,6 +135,7 @@ app.get('/container', express.async(async (req, res) => {
 app.delete('/container/:id', express.async(async (req, res) => {
   var {id} = req.params;
   res.json(await container.remove(id, req.body));
+  containerPost(id);
 }));
 app.get('/container/:id/status', express.async(async (req, res) => {
   var {id} = req.params, write = boolean(req.body.write);
@@ -100,10 +144,12 @@ app.get('/container/:id/status', express.async(async (req, res) => {
 app.post('/container/:id/status', express.async(async (req, res) => {
   var {id} = req.params;
   res.json(await container.setStatus(id, req.body));
+  containerPost(id);
 }));
 app.post('/container/:id/exec', express.async(async (req, res) => {
   var {id} = req.params;
   res.json(await container.exec(id, req.body));
+  containerPost(id);
 }));
 app.get('/container/:id/export', express.async(async (req, res) => {
   var {id} = req.params;
@@ -113,6 +159,7 @@ app.get('/container/:id/export', express.async(async (req, res) => {
 app.all('/container/:id/:action', express.async(async (req, res) => {
   var {id, action} = req.params;
   res.json(await container.command(id, action, req.body));
+  containerPost(id);
 }));
 
 app.use((err, req, res, next) => {
