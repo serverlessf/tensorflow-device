d0cca1f7687a5070466ad9cba5e42b0b38959c0e
Subhajit Sahu <wolfram77@gmail.com>
2019-04-17 16:00:44 +0530

    :mushroom: simplify: config.js

--- src/config.js
+++ src/config.js
@@ -3,23 +3,16 @@ const path = require('path');
 
 
 
-const FILE = 'config.json';
-
-
-
 async function read(file) {
-  // var file = path.join(dir, FILE);
   if(!fs.existsSync(file)) return {};
   var data = await fs.readFile(file, 'utf8');
   return JSON.parse(data);
 }
 
 async function write(file, value) {
-  // var file = path.join(dir, FILE);
   await fs.mkdirp(path.dirname(file));
   var value = Object.assign(await read(file), value);
   await fs.writeFile(file, JSON.stringify(value, null, 2));
 }
-exports.FILE = FILE;
 exports.write = write;
 exports.read = read;
